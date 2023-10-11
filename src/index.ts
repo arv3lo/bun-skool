57ae8585b054b52b4eea9f88f0d6120f576e9d0b
gérald <arivelo.live@gmail.com>
2023-10-11 22:32:48 +0300

    added basic configs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Elysia } from "elysia";
 import routes from "./routes";
-
+const PORT = process.env.PORT || 5000;
 const app = new Elysia();
 routes(app);
 
-app.listen(3000);
+app.listen(PORT);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port} ...`
 );
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Elysia } from "elysia";
 import usersRoutes from "./usersRoutes";
-// =>
 
 const routes = (app: Elysia) => {
   app.group("/users", app => usersRoutes(app));
